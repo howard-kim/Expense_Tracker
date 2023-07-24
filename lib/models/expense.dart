5236f6c9bd2b8626f582080ff6d8522752ca3144
Dong-Wan Kim <docong0120@gmail.com>
2023-07-24 18:05:09 +0900

    Just implemented TextField, 유저인풋 받을 수 있음

--- lib/models/expense.dart
+++ lib/models/expense.dart
@@ -1,8 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart'; //intl 패키지 안에 시간 관련 여러가지 기능이 존재
 import 'package:uuid/uuid.dart';
+
+final formatter =
+    DateFormat.yMd(); //이것으로 포매터의 형식을 지정해주는 것 DateFormat안에는 여러종류의 형식이 존재
 
 const uuid = Uuid();
 
 enum Category { food, trabel, leisure, work }
+
+const categoryIcons = {
+  Category.food: Icons.lunch_dining,
+  Category.leisure: Icons.movie,
+  Category.trabel: Icons.flight_takeoff,
+  Category.work: Icons.work,
+};
 
 class Expense {
   //하나의 비용에 대한 구조체
@@ -18,4 +30,8 @@ class Expense {
   final double amount;
   final DateTime date;
   final Category category;
+
+  String get formattedDate {
+    return formatter.format(date);
+  }
 }
--- lib/widgets/expenses_list/expenses_list.dart
+++ lib/widgets/expenses_list/expenses_list.dart
@@ -1,4 +1,5 @@
 import 'package:expense_tracker/models/expense.dart';
+import 'package:expense_tracker/widgets/expenses_list/expense_item.dart';
 import 'package:flutter/widgets.dart';
 
 class ExpensesList extends StatelessWidget {
@@ -12,9 +13,7 @@ class ExpensesList extends StatelessWidget {
   Widget build(BuildContext context) {
     return ListView.builder(
       itemCount: expenses.length,
-      itemBuilder: (context, index) => Text(
-        expenses[index].title,
-      ),
+      itemBuilder: (context, index) => ExpenseItem(expense: expenses[index]),
     );
   }
 }
